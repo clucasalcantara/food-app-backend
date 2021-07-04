5eaf11bc2727ffd1a2f73a2128c4e35cfcf88052
Caio Alcantara <alcantaracaiolucas@gmail.com>
2021-07-04 19:42:18 -0300

    feat: add auth middleware

--- src/main.js
+++ src/main.js
@@ -3,21 +3,42 @@
  */
 import Koa from 'koa'
 import cors from 'koa2-cors'
-import { ApolloServer } from 'apollo-server-koa'
+import { ApolloServer, AuthenticationError } from 'apollo-server-koa'
 import logger from 'hoopa-logger'
+import { verify } from 'jsonwebtoken'
 // GraphQL Definitions
 import { appSchema as schema } from './graphql'
 
-const Server = new ApolloServer({ schema })
-const Athena = new Koa()
+const Server = new ApolloServer({
+  schema,
+  context: ({ ctx: { request } }) => {
+    const {
+      header: { authorization },
+    } = request
+
+    try {
+      verify(authorization, process.env.API_SECRET)
+    } catch (error) {
+      const { origin } = request
+
+      logger.error(
+        `Unauthenticated request from ${origin}, user-agent: ${request.header['user-agent']}`
+      )
+
+      throw new AuthenticationError(error)
+    }
+  },
+})
+
+const EatList = new Koa()
 
 const port = process.env.PORT || 4005
 const { graphqlPath } = Server
 
-Athena.use(cors())
-Server.applyMiddleware({ app: Athena })
+EatList.use(cors())
+Server.applyMiddleware({ app: EatList })
 
-Athena.listen({ port }, () =>
+EatList.listen({ port }, () =>
   logger.info(
     `🚀 Food App API running at http://localhost:${port}${graphqlPath}`
   )
