1c4e7eaf3bac7236ab884b682de1dca5af942c8f
Caio Alcantara <alcantaracaiolucas@gmail.com>
2021-07-07 21:23:32 -0300

    feat: add rest route to passport auths

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
  * Server instance
  */
 import Koa from 'koa'
+import Router from 'koa-router'
 import cors from 'koa2-cors'
 import { ApolloServer } from 'apollo-server-koa'
 import logger from 'hoopa-logger'
@@ -18,11 +19,21 @@ const Server = new ApolloServer({
 })
 
 const EatList = new Koa()
+const router = new Router()
+
+router.get('/auth/facebook', (ctx, next) => {
+  // @TODO: Authenticate using passport
+  ctx.status = 200
+  ctx.body = 'In development'
+  next()
+})
 
 const port = process.env.PORT || 4005
 const { graphqlPath } = Server
 
 EatList.use(cors())
+EatList.use(router.routes()).use(router.allowedMethods())
+
 Server.applyMiddleware({ app: EatList })
 
 EatList.listen({ port }, () =>
