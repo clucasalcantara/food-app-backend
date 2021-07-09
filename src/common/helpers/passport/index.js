da98dd44759be160cc18a302ef6634f010773b66
Caio Alcantara <alcantaracaiolucas@gmail.com>
2021-07-09 15:17:26 -0300

    fix: facebook login

--- src/common/helpers/passport/index.js
+++ src/common/helpers/passport/index.js
@@ -11,22 +11,12 @@ export const usePassport = () => {
       {
         clientID: process.env.FB_APP_ID,
         clientSecret: process.env.FB_APP_SECRET,
-        callbackURL: 'https://api.foodapp.caioalcantara.dev/auth/facebook',
+        callbackURL:
+          'https://api.foodapp.caioalcantara.dev/auth/facebook/callback',
         profileFields: ['email', 'name'],
       },
       (accessToken, refreshToken, profile, done) => {
-        const { email, first_name, last_name } = profile._json
-
-        const userData = {
-          email,
-          firstName: first_name,
-          lastName: last_name,
-          accessToken,
-          refreshToken,
-        }
-
-        console.log({ userData })
-        done(null, profile)
+        return done(null, { profile, accessToken, refreshToken })
       }
     )
   )
--- src/rest-auth/index.js
+++ src/rest-auth/index.js
@@ -1,11 +1,25 @@
 export const useRestRoutes = (router, passport) => {
+  router.get('/auth/facebook', passport.authenticate('facebook'))
+
   router.get(
-    '/auth/facebook',
-    passport.authenticate('facebook', {
-      successRedirect: '/facebook/success',
-      failureRedirect: '/facebook/failure',
-    })
+    '/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/auth/failure' }),
+    (ctx, next) => {
+      // @TODO: findOrCreate User
+      ctx.body = {
+        user: ctx.req.user,
+      }
+
+      next('/home')
+    }
   )
+
+  router.get('/auth/failure', ctx => {
+    ctx.body = {
+      status: 403,
+      message: 'Login attempt failed',
+    }
+  })
 
   return router
 }
